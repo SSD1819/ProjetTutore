d568f8af83b900cc7633f5686d7b5293da9f2edc
Matthieu F <matthieu.francois@yahoo.fr>
2019-01-29 23:00:59 +0100

    Maj de l'arbre pour vec et score + ajout des 2 tables en .rdata

--- arbre_deci.R
+++ arbre_deci.R
@@ -1,30 +1,48 @@
-# install.packages("tree")
-require(tree)
-
-#Remplacement des NAs par une valeur "NA" comme une modalité pour l'arbre
-dataTree<-dataPropre
-#Premier arbre de décision pour voir
-summary(dataTree)
+######
+#SUR VECTEUR
+######
+summary(dataVec)
 
 # install.packages("rpart")
 # install.packages("rpart.plot")
 require(rpart)# Pour l’arbre de décision
 require(rpart.plot) # Pour la représentation de l’arbre de décision
 
-colnames(dataTree)
-quest<-c("T21.TOTAL","T22.TOTAL","T23.TOTAL","T31TOTAL",
-         "T32.TOTAL","T41a.TOTAL","T41b.TOTAL","T41cTOTAL",
-         "T41d.TOTAL","T42a.TOTAL","T42b.TOTAL","T42c.TOTAL",
-         "T42d.TOTAL","T51.TOTAL","T52.TOTAL","T61.TOTAL",
-         "T62TOTAL","T71.TOTAL","T72TOTAL","T81.TOTAL","T82.TOTAL",
-         "T83.TOTAL","T84.TOTAL","T85.TOTAL","T86.TOTAL",
-         "T87.TOTAL","T88.TOTAL","T89.TOTAL","T91.Total",
-         "T92.Total","T111.TOTAL","T112.TOTAL","T113.TOTAL")
-don.reg<-dataTree[,c("Pédagogie",quest)]
-don.tree <- rpart(Pédagogie~.,data=don.reg,method= "class", control=rpart.control(minsplit=5,cp=0))
-
+colnames(dataVec)
+quest<-c("Sexe","Classe.d.age","T1","T2","T3",
+         "T4","T5","T6","T7",             
+         "T81.TOTAL","T82.TOTAL","T83.TOTAL","T84.TOTAL",
+         "T85.TOTAL","T86.TOTAL","T87.TOTAL","T88.TOTAL",
+         "T89.TOTAL","T9","T11" )
+don.reg<-dataVec[,c("Pédagogie",quest)]
+don.tree <- rpart(Pédagogie~.,data=don.reg,method= "class", control=rpart.control(minsplit=1,cp=0))
 #Affichage du résultat
 plot(don.tree, uniform=TRUE, branch=0.5, margin=0.1)
 text(don.tree, all=FALSE, use.n=TRUE,cex=0.7)
-
+# prp(don.tree,extra = 1)
 plotcp(don.tree)
+
+tree.opt<-prune(don.tree,cp=don.tree$cptable[which.min(don.tree$cptable[,4]),1])#
+prp(tree.opt,extra = 1)
+
+######
+#SUR SCORE
+######
+summary(dataSum)#Aucun score en quantitatif ? et encore des NA dans la T11
+colnames(dataSum)
+quest<-c("Sexe","Classe.d.age","T1","T2","T3",
+         "T4","T5","T6","T7",             
+         "T81.TOTAL","T82.TOTAL","T83.TOTAL","T84.TOTAL",
+         "T85.TOTAL","T86.TOTAL","T87.TOTAL","T88.TOTAL",
+         "T89.TOTAL","T9","T11" )
+don.reg<-dataSum[,c("Pédagogie",quest)]
+don.tree <- rpart(Pédagogie~.,data=don.reg,method= "class", control=rpart.control(minsplit=1,cp=0))
+#Affichage du résultat
+plot(don.tree, uniform=TRUE, branch=0.5, margin=0.1)
+text(don.tree, all=FALSE, use.n=TRUE,cex=0.7)
+# prp(don.tree,extra = 1)
+plotcp(don.tree)
+
+tree.opt<-prune(don.tree,cp=don.tree$cptable[which.min(don.tree$cptable[,4]),1])#
+prp(tree.opt,extra = 1)
+

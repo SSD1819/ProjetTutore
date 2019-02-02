cc29ba5a0501e3ad2379498e6c2304edea72e796
Etienne <etienne@Etiennes-MacBook-Air.local>
2019-02-02 19:33:27 +0100

    MCA sans T9 et T11

--- ACM.R
+++ ACM.R
@@ -1,30 +1,25 @@
-dataMoySec_clean<-dataMoySec[,colSums(sapply(lapply(dataMoySec,is.na),as.numeric))==0] #Suppression des colonnes avec NA
-#NB colonne total : NA non compté comme des NA
-dataMoySec_clean<-dataMoySec_clean[,-c(ncol(dataMoySec_clean)-1)]#suppression de cette colonne donc
-summary(dataMoySec_clean)
-nom<-colnames(dataMoySec_clean)[c(1,2,3,4,9,11:ncol(dataMoySec_clean)-1)]
+colnames(dataPropre)
 
-for (i in 1:ncol(dataMoySec_clean)){
-  if (colnames(dataMoySec_clean)[i]%in%nom ){
-    dataMoySec_clean[,i]<-as.factor(dataMoySec_clean[,i])
-  }
-}
-dim(valquali)
-valquali<-dataMoySec_clean[,c(2,4,ncol(dataMoySec_clean),9,11:ncol(dataMoySec_clean)-1)]
-# valquali<-dataMoySec_clean[,c(2,12:ncol(dataMoySec_clean)-1)]
+nom<-colnames(dataPropre[,c(2,11,13:41)])
+valquali<-dataPropre[,nom]
+
 summary(valquali)
-library(FactoMineR)
-res.mca<-MCA(valquali,quali.sup = 1)
+require(FactoMineR)
+require(factoextra)
+
+res.mca<-MCA(valquali,quali.sup = 1,quanti.sup = 2:3)
 dimdesc(res.mca)
 summary(res.mca)
-plot.MCA(res.mca,invisible = "ind",cex=0.7,selectMod =  "contrib 15",axes = c(3,4))
+plot.MCA(res.mca,invisible = "ind",cex=0.7,selectMod =  "contrib 15")
 plot.MCA(res.mca,invisible = "ind",cex=0.7,selectMod =  "cos2 10")
-plot.MCA(res.mca,choix = "var",cex=0.7,axes = c(3,4))
-
-plot.MCA(res.mca,invisible = "ind",cex=0.7,axes = c(1,2))
+plot.MCA(res.mca,choix = "var",cex=0.7,axes = c(1,2))
 plot.MCA(res.mca,invisible = "ind",cex=0.7,axes = c(1,2))
 
 plot.MCA(res.mca,choix = "ind",cex=0.7,invisible = "ind")
 res.hcpc<-HCPC(res.mca,nb.clust = 2)
 res.hcpc$desc.var
 plot.HCPC(res.hcpc,choice = "bar")
+
+rm(res.hcpc, res.mca, valquali, nom)
+
+

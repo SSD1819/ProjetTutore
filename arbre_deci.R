0e973607c279844b1e583e1992796079ee2216da
Matthieu F <matthieu.francois@yahoo.fr>
2019-02-04 13:14:46 +0100

    ajustements regression

--- arbre_deci.R
+++ arbre_deci.R
@@ -15,10 +15,10 @@ summary(dataVec)
 require(rpart)# Pour l’arbre de décision
 require(rpart.plot) # Pour la représentation de l’arbre de décision
 
-colnames(dataVec)
+colnames(dataPropre)
 quest<-c("Sexe","AgeInt","T1", "T21","T22","T23","T31","T32",
-         "T41","T41d","T42a","T42b","T42c","T42d",
-         "T51","T52","T61","T6","T71","T7" ,             
+         "T41c","T41b","T41a","T41d","T42a","T42b","T42c","T42d",
+         "T51","T52","T61","T62","T71","T72" ,             
          "T81","T82","T83","T84",
          "T85","T86","T87","T88",
          "T89")
@@ -26,10 +26,13 @@ summary(dataPropre)
 don.reg<-dataPropre[,c("Pedagogie",quest)]
 
 #essaie de passer par la reg logistique pour une cp normale
-reg<-glm(Pedagogie~T22+T42c+T71+T81+T41+T89,data=don.reg,family = binomial)
-reg<-glm(Pedagogie~T41+T89,data=don.reg,family = binomial)#T42c limite
-reg<-glm(Pedagogie~.,data=don.reg,family = binomial)
+reg<-glm(Pedagogie~T22+T41c+T42c+T71+T81+T89,data=don.reg,family = binomial(link = "logit"))
+reg<-glm(Pedagogie~T41c+T89,data=don.reg,family = binomial(link = "logit"))#T42c limite
+# reg<-glm(Pedagogie~.,data=don.reg,family = binomial)
 summary(reg)
+
+exp(-0.6028+-0.7822*0+1.0380)/(1+exp(-0.6028+-0.7822*0+1.0380))
+
 plotcp(rpart(reg))
 
 don.tree <- rpart(reg)
@@ -43,3 +46,4 @@ tree.opt<-prune(don.tree,cp=0.01)#
 prp(tree.opt,type=4,extra = 1)
 
 rm(don.tree,reg,don.reg,tree.opt)
+

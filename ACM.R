71e1f24c89bcdd2efe7c0d596f47277350723b13
Etienne <etienne@wificampus-028099.grenet.fr>
2019-02-06 15:07:20 +0100

    T1 en Quali Sup

--- ACM.R
+++ ACM.R
@@ -1,17 +1,18 @@
 colnames(dataPropre)
 
-nom<-colnames(dataPropre[,c(2,11,13:41)])
+nom<-colnames(dataPropre[,c(2,13,11,14:41)])
 valquali<-dataPropre[,nom]
 
 summary(valquali)
 require(FactoMineR)
 require(factoextra)
+valquali$T1<- as.factor(valquali$T1)
 
-res.mca<-MCA(valquali,quali.sup = 1,quanti.sup = 2:3)
+res.mca<-MCA(valquali,quali.sup = 1:2,quanti.sup = 3)
 dimdesc(res.mca)
 summary(res.mca)
 plot.MCA(res.mca,invisible = "ind",cex=0.7,selectMod =  "contrib 15")
-plot.MCA(res.mca,invisible = "ind",cex=0.7,selectMod =  "cos2 10")
+plot.MCA(res.mca,invisible = "ind",cex=0.7)
 plot.MCA(res.mca,choix = "var",cex=0.7,axes = c(1,2))
 plot.MCA(res.mca,choix = "ind",invisible="ind",cex=0.7,axes = c(1,2))#effet gutman !
 #
@@ -28,7 +29,7 @@ res.hcpc<-HCPC(res.mca,nb.clust = 10)
 table(res.hcpc$data.clust$T1,res.hcpc$data.clust$clust)
 #semble y avoir sur T1 les groupes suivants : 3 - 4 - 5 - 10 - 16 - 20
 
-rm(res.hcpc, res.mca, valquali, nom)
+#rm(res.hcpc, res.mca, valquali, nom)
 
 
 

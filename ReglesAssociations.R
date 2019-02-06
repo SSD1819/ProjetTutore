a71a2bddd8ba2ae558c9d08a4cb21d88fe62a37a
Lucas Chabeau <chabeaul@f217-01.e-im2ag.ujf-grenoble.fr>
2019-02-06 15:30:07 +0100

    Regles d'association à améliorer

--- ReglesAssociations.R
+++ ReglesAssociations.R
@@ -13,8 +13,51 @@ p2[which(dataPropre$Pedagogie=="P2")]<-"1"
 #On ajoute p1 et p2
 datAssos<-cbind(p1,p2,dataPropre[,14:41])
 
+#Transformation de datassos en tableau de variables numériques
+datAssos<-apply(datAssos,2,as.numeric)
+
+#### Règles d'associations avec fonction Apriori() ####
 rules <- apriori(datAssos, parameter = list(supp = 0.5, conf = 0.9, target = "rules"))
 summary(rules)
 rules_conf <- sort (rules, by="confidence", decreasing=TRUE) # 'high-confidence' rules.
 inspect(head(rules_conf,150))
-rm(datAssos,p1,p2)
+
+
+#### Test avec la méthode du TP3 Applied proba ####
+
+#1-item support
+support1<-apply(X=datAssos,MARGIN=2,FUN=mean)
+support1
+
+#2-item support
+support2<-t(datAssos)%*%datAssos
+support2<-support2/length(datAssos[,1])
+support2
+
+#Confidence
+confidence<-support2/support1
+confidence
+
+#lift
+lift<-support2/(support1%*%t(support1))
+lift
+
+###Règles
+suppMin<-0.5
+confMin<-0.5
+liftMin<-1.1
+
+#Matrice des règles d'associations
+reglesMat<-(support2>=suppMin)*(confidence>=confMin)*(lift>=liftMin)
+
+#Suppression des associations inutiles du type A=>A
+diag(reglesMat)<-0
+
+#Règles écrites (et non en matrice)
+posSignif<-which(reglesMat==1)
+iSignif<-posSignif%%length(reglesMat[,1])
+jSignif<-ceiling(posSignif/length(reglesMat[,1]))
+regles<-paste(rownames(reglesMat[iSignif,jSignif]),"=>",colnames(reglesMat[iSignif,jSignif]),sep=" ")
+regles
+
+rm(datAssos,p1,p2,support1,support2,confidence,lift,suppMin,confMin,liftMin,reglesMat,posSignif,iSignif,jSignif,regles, rules, rules_conf)

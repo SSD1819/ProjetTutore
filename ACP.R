b23d7ae4f500fe7af7164d83eb59c243e82059e2
Matthieu F <matthieu.francois@yahoo.fr>
2019-02-07 14:35:04 +0100

    ajout acp sans q8

--- ACP.R
+++ ACP.R
@@ -10,12 +10,13 @@ noms<-c("Pedagogie", "T1","T2","T3",
         "T82","T83","T84","T85","T86","T87",
         "T88","T89")
 valquanti<-dataSum[,noms]
+valquanti[,2:length(valquanti)]<-scale(valquanti[,2:length(valquanti)])
 res.pca<-PCA(valquanti,quali.sup = 1)
 plot.PCA(res.pca,choix = "var",select = "contrib 8") # à nouveau les questions 8 ressortent le plus (puis la 3)
 
 # install.packages("corrplot")
 library(corrplot)
-m.cor<-cor(valquanti[,(ncol(valquanti)-9):ncol(valquanti)])#matrice des corrélations pour les questions 8
+m.cor<-cor(valquanti[,2:ncol(valquanti)])#matrice des corrélations pour les questions 8
 corrplot(m.cor,method = "circle")
 #grosse corrélation entre les 8* : donc ACP biaisée
 #Etonnemment la T1 ne ressort pas comme grosse contrib
@@ -23,11 +24,20 @@ corrplot(m.cor,method = "circle")
 plot.PCA(res.pca,choix = "var",select = "contrib 8",axes = c(3,4)) #Dim 3 > T_72
 summary(res.pca)
 
+##Nouvelle PCA sans les T8 super correlées entre elles (que T81 car non correlée)
+valquanti1<-valquanti[,-c(11:18)]
+summary(valquanti1)
+res.pca1<-PCA(valquanti1,quali.sup = 1)
+plot.PCA(res.pca1,axes = c(1,2),choix = "ind")#aucune démarcation entre P1 et P2
+plot.PCA(res.pca1,axes = c(1,2),choix = "var",select = "cos2 5")#T2 / T3 proche : gros score en T2 > gros score en T3
+#D1 : T2 | T3
+#D2 : T62 
+plot.PCA(res.pca1,axes = c(3,4),choix = "var")
+#D3 : T72
+#D4 : T5
+#quasi 1 variable / axe > peu utile 
 
-#test de PCA sans la T1 pour peut etre plus d'info
-res.pca1<-PCA(valquanti,quali.sup = 1,quanti.sup = 2)
+summary(res.pca1)
 
-
-
-
+rm(res.pca,res.pca1,valquanti)
 

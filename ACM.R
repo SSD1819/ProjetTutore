6ef00ecb94a5818be8f011f1e25b049932036b44
Matthieu F <matthieu.francois@yahoo.fr>
2019-02-07 11:35:18 +0100

    ajout de commentaire pour décrire acm / cah

--- ACM.R
+++ ACM.R
@@ -1,6 +1,7 @@
 colnames(dataPropre)
 
-nom<-colnames(dataPropre[,c(2,13,14:41)])
+nom<-colnames(dataPropre[,c(2,13,14:41)])#suppression de la variable quanti sur l'age (qui ne ressortait pas de toute façon) car problème lors d'ajout d'individus supplémentaire
+
 valquali<-dataPropre[,nom]
 
 summary(valquali)
@@ -14,24 +15,29 @@ rownames(valquali)[nrow(valquali)]<-"ind_0"
 valquali[nrow(valquali)+1,]<-c("P1",0,rep(1,28))
 rownames(valquali)[nrow(valquali)]<-"ind_1"
 summary(valquali)
-table(valquali$T1)
 
 
+#ACM
 res.mca<-MCA(valquali,quali.sup = 1:2,ind.sup = 157:158)
 dimdesc(res.mca)
 summary(res.mca)
 plot.MCA(res.mca,invisible = "ind",cex=0.7,selectMod =  "contrib 15")
 plot.MCA(res.mca,invisible = "ind",cex=0.7)
-plot.MCA(res.mca,choix = "var",cex=0.7,axes = c(1,2))
-plot.MCA(res.mca,choix = "ind",invisible="ind",cex=0.7,axes = c(1,2))#effet gutman !
-#
-plot.MCA(res.mca,invisible = "ind",cex=0.7,axes = c(1,2))
+#Dim1 : mauvais - bons
+#Dim2 : T4 - T8
+plot.MCA(res.mca,choix = "ind",cex=0.7,axes = c(1,2),invisible = c("ind","var"))
+#faible séparation entre P1 et P2 selon la dim1, 0 sur dim2 : pas de diff concrête
 
-plot.MCA(res.mca,choix = "ind",cex=0.7,invisible = "ind")
 res.hcpc<-HCPC(res.mca,nb.clust = 3)
 res.hcpc$desc.var
 plot.HCPC(res.hcpc,choice = "bar")
+# C1 : Echec aux q84 85 87 89 88 ... 
+# C2 : Réussite aux q84 82 85 .. 22 mais echec aux q42a-d-c-b-a
+# C3 : Réussite aux q4 31 32  8
 
+## > 1 classe d'inds "mauvais" 
+## > 1 de bons pour les 8 mais mauvais pour les 4
+## > 1 de bons
 
 ####Clustering de la question 1
 res.hcpc<-HCPC(res.mca,nb.clust = 10)

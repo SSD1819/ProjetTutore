dd2d215f0d6762872a5ac4b576482c29ad2190b7
Azat <azatalex@yahoo.com>
2019-02-12 15:20:05 +0100

    acm modifs

--- ACM-dataVec.R
+++ ACM-dataVec.R
@@ -2,23 +2,58 @@
 
 colnames(dataVec)
 
-noms<-colnames(dataVec[,c(2,14,11,15:23)]) #removed T8
+noms<-colnames(dataVec[,c(2,14,11,15:32)]) #removed T83 et T85+
 valqualis<-dataVec[,noms]
 summary(valqualis)
 
-### To find correlations
+valqualis$T1<- as.factor(valqualis$T1)
+valqualis$AgeNum<- as.factor(valqualis$AgeNum)
+
+require(FactoMineR)
+require(factoextra)
+
+res.mca<-MCA(valqualis,quali.sup = 1:2)
+
+#6.1% information
+
+### To find correlation between only T8 questions
+
+dataVec->dataVec1
+for( i in 24:32){
+  dataVec1[,i]<-as.numeric(dataVec1[,i])
+}
+str(dataVec1)
+cor(dataVec1[,24:32])
+
+#install.packages("corrplot")
+require(corrplot)
+corrplot(cor(dataVec1[,24:32]))
+
+#removing T82+
+
+noms<-colnames(dataVec[,c(2,14,11,15:25)]) #removed T82+
+valqualis<-dataVec[,noms]
+summary(valqualis)
+
+valqualis$T1<- as.factor(valqualis$T1)
+valqualis$AgeNum<- as.factor(valqualis$AgeNum)
+
+res.mca<-MCA(valqualis,quali.sup = 1:2)
+
+#Here we have 4.37% informations. Trying to reduce variables.
+
+#Again correlations
 
 valqualis->valqualis1
-for( i in 1:12){
+for( i in 1:14){
   valqualis1[,i]<-as.numeric(valqualis1[,i])
 }
 str(valqualis1)
 cor00<-cor(valqualis1)
 
 #install.packages("corrplot")
-require(corrplot)
+#require(corrplot)
 corrplot(cor(cor00))
-###
 
 #T2 and T3 are correlated, as well as T41 and T42, so dropping T3 and T42
 
@@ -26,7 +61,7 @@ valqualis<-valqualis[,-c(5,7)]
 
 ###rechecking correlations
 valqualis->valqualis1
-for( i in 1:10){
+for( i in 1:12){
   valqualis1[,i]<-as.numeric(valqualis1[,i])
 }
 str(valqualis1)
@@ -38,14 +73,9 @@ corrplot(cor(cor000))
 
 ### Continue ACM
 
-require(FactoMineR)
-require(factoextra)
-valqualis$T1<- as.factor(valqualis$T1)
-valqualis$AgeNum<- as.factor(valqualis$AgeNum)
-
 res.mca<-MCA(valqualis,quali.sup = 1:2)
 
-###Here he have only 4.14% information kept! Which is terribly low!
+###Here he have only 3.98% information kept! Which is terribly low!
 
 dimdesc(res.mca)
 summary(res.mca)

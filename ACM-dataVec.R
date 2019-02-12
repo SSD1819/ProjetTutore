c8311b70a03954a96e25eb18a5c41135ca571d2c
Azat <azatalex@yahoo.com>
2019-02-12 14:42:10 +0100

    dataVec ACM finit, pas de resultat. Besoin de point de vue d'expert

--- ACM-dataVec.R
+++ ACM-dataVec.R
@@ -1,28 +1,40 @@
-
-### PAS FINIT ###
-
 ### ACM sur data vectorielle
 
 colnames(dataVec)
 
-noms<-colnames(dataVec[,c(2,14,11,15:25,27)]) #removed T83 et T85+
+noms<-colnames(dataVec[,c(2,14,11,15:23)]) #removed T8
 valqualis<-dataVec[,noms]
 summary(valqualis)
 
-### To find correlation between questions T8
+### To find correlations
 
-dataVec->dataVec1
-for( i in 24:32){
-  dataVec1[,i]<-as.numeric(dataVec1[,i])
+valqualis->valqualis1
+for( i in 1:12){
+  valqualis1[,i]<-as.numeric(valqualis1[,i])
 }
-str(dataVec1)
-cor(dataVec1[,24:32])
+str(valqualis1)
+cor00<-cor(valqualis1)
 
 #install.packages("corrplot")
 require(corrplot)
-corrplot(cor(dataVec1[,24:32]))
-
+corrplot(cor(cor00))
 ###
+
+#T2 and T3 are correlated, as well as T41 and T42, so dropping T3 and T42
+
+valqualis<-valqualis[,-c(5,7)]
+
+###rechecking correlations
+valqualis->valqualis1
+for( i in 1:10){
+  valqualis1[,i]<-as.numeric(valqualis1[,i])
+}
+str(valqualis1)
+cor000<-cor(valqualis1)
+
+#install.packages("corrplot")
+#require(corrplot)
+corrplot(cor(cor000))
 
 ### Continue ACM
 
@@ -31,8 +43,9 @@ require(factoextra)
 valqualis$T1<- as.factor(valqualis$T1)
 valqualis$AgeNum<- as.factor(valqualis$AgeNum)
 
+res.mca<-MCA(valqualis,quali.sup = 1:2)
 
-res.mca<-MCA(valqualis,quali.sup = 1:3)
+###Here he have only 4.14% information kept! Which is terribly low!
 
 dimdesc(res.mca)
 summary(res.mca)
@@ -44,6 +57,5 @@ res.hcpc<-HCPC(res.mca,nb.clust = 3)
 res.hcpc$desc.var
 plot.HCPC(res.hcpc,choice = "bar")
 
-
-rm(i,res.hcpc, res.mca, valqualis, noms, dataVec1)
-
+#Removing temporary variables
+rm(i,res.hcpc, res.mca, valqualis, noms, dataVec1, cor00, cor000, valqualis1)

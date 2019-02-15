39e73e1d348fa5ba9cec533b3eba8d9b561c47a4
Matthieu F <matthieu.francois@yahoo.fr>
2019-02-15 11:41:01 +0100

    acp avec ind sup

--- ACP.R
+++ ACP.R
@@ -6,24 +6,32 @@ summary(dataSum)
 #soit afm
 colnames(dataSum)
 noms<-c("Pedagogie", "T1","T2","T3",
-        "T5","T61","T62","T71","T72","T81",
-        "T82","T83","T84","T85","T86","T87",
-        "T88","T89")
+        "T5","T61","T62","T71","T72",
+        "T8.123","T8456789")
 valquanti<-dataSum[,noms]
 valquanti[,2:length(valquanti)]<-scale(valquanti[,2:length(valquanti)])
 res.pca<-PCA(valquanti,quali.sup = 1)
-plot.PCA(res.pca,choix = "var",select = "contrib 8") # à nouveau les questions 8 ressortent le plus (puis la 3)
+plot.PCA(res.pca,choix = "var",select = "contrib 4") # à nouveau les questions 8 ressortent le plus (puis la 3)
+plot.PCA(res.pca,choix = "ind",cex=0.7)
+outlier<-head(rownames(res.pca$ind$coord[order(res.pca$ind$coord[,2], res.pca$ind$coord[,1]),]))
+
+
+#sans les individus athypiques
+don.indsup<-valquanti[!rownames(valquanti)%in%outlier,]
+don.indsup<-rbind(don.indsup,valquanti[outlier,])
+res.don<-PCA(don.indsup,quali.sup = 1,ind.sup = (nrow(don.indsup)-6):nrow(don.indsup))
+plot.PCA(res.don,choix = "ind",cex=0.7,axes = c(3,4))
+plot.PCA(res.don,choix = "var",select = "contrib 6")
+summary(res.don)
 
 # install.packages("corrplot")
 library(corrplot)
 summary(dataPropre)
-m.cor<-cor(sapply(dataPropre[,13:ncol(dataPropre)],as.numeric))#matrice des corrélations pour les questions 8
+m.cor<-cor(valquanti[,-1])#matrice des corrélations pour les questions 8
 corrplot(m.cor,method = "circle")
 #grosse corrélation entre les 8* : donc ACP biaisée (et légère sur les q4 mais suffisante pour biaiser l'analyse)
 #Etonnemment la T1 ne ressort pas comme grosse contrib
 #seulement 60% de l'info sur les 4 premières dimensions
-plot.PCA(res.pca,choix = "var",select = "contrib 8",axes = c(3,4)) #Dim 3 > T_72
-summary(res.pca)
 
 ##Nouvelle PCA sans les T8 super correlées entre elles (que T81 car non correlée)
 valquanti1<-valquanti[,-c(12:18)]

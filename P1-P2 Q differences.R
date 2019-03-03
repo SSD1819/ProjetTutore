f10ad75313f5969d3385d7d2d92c0b777c274344
Azat <azatalex@yahoo.com>
2019-03-03 23:06:28 +0100

    Creation of new script-file of differences tests taken from stat. univarie. To be checked

--- P1-P2 Q differences.R
+++ P1-P2 Q differences.R
@@ -1,40 +1,3 @@
-library(plyr)
-
-#fonction prop.table(table)
-TablesUniv<-function(x){
-  return(prop.table(table(x)))
-}
-
-#prop de toutes les colonnes
-analyse.univ<-apply(X=dataPropre,MARGIN=2,FUN=TablesUniv)
-
-#effectif de toutes les colonnes
-analyse.univ.table<-apply(X=dataPropre,MARGIN=2,FUN=table)
-
-#Representation des Droitiers et Gauchers
-pie(analyse.univ$Lateralite, labels = c("Utilise les deux pour dessiner","Droitier", "Gaucher"))
-
-#Representation des langues parlées pr les élèves
-pie(analyse.univ.table$Langues, labels1 = names(analyse.univ.table$Langues))
-
-#Analyse Question T1 "Vous savez compter jusqu'à combien?"
-plot(analyse.univ.table$T1[order(analyse.univ.table$T1)],type="h", 
-     xlab="Limite de comptage", ylab="Nombre d'élèves", main="Histogramme ")
-analyse.univ.table$T1
-
-#Comptes des résultats
-
-apply(X=dataPropre[,16:42],MARGIN=1,FUN=plyr::count)
-
-names(dataPropre)
-
-#Represantation des differents types de pédagogies au sein de l'ecole
-pie(analyse.univ.table$Pedagogie)
-analyse.univ$Pédagogie
-
-#Majorité de P1
-
-########
 #Resultats univarie en comparant les 2 pedagogies
 
 dataPropreP1<-subset(dataPropre, Pedagogie == "P1")
@@ -132,4 +95,3 @@ prop.test(table(dataPropreP2$T52), alternative = "two.sided")
 
 
 rm(list=setdiff(ls(), c("dataPropre", "dataSum", "dataVec")))
-

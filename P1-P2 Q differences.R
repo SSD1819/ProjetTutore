68c1bcb60906e7f0c022f5d9379e2da8ace1aeeb
Azat <azatalex@yahoo.com>
2019-03-04 09:27:50 +0100

    info addition

--- P1-P2 Q differences.R
+++ P1-P2 Q differences.R
@@ -1,5 +1,22 @@
 # P1-P2 Questions differences: tests and visualisation
 
+### Information about correct statement in test functions ###
+## chi-square test##
+
+# The 'correct=FALSE' option in the chisq.test function turns off Yates' correction for
+# the chi-square test (which is used with small sample sizes), and gives the standard chi-square test 
+# statistic. R gives a two-tailed p-value. Note that the title for the output,
+# 'Pearson's Chi-squared test' indicates that these results are for the uncorrected (not Yates'
+# adjusted) chi-square test.
+
+## prop.test ##
+
+# To use the usual large-sample formula in calculating the confidence interval,
+# include the 'correct=FALSE' option to turn off the small sample size correction factor 
+# in the calculation (although in this example, with only 17 subjects in the control group, 
+# the small sample version of the confidence interval might be more appropriate).
+
+                                                                                                                                                                                        
 # Creation of dataframes containing only respectively P1 and P2 
 
 dataPropreP1<-subset(dataPropre, Pedagogie == "P1")
@@ -26,7 +43,7 @@ T22 <- rbind(prop.table(table(dataPropreP1$T22)),prop.table(table(dataPropreP2$T
 barplot(T22, beside = T, col = c("blue", "green"), main="T22 reponses",
         legend.text =c("P1","P2"),args.legend = list(x = "top"))
 
-chi2<-chisq.test(table(dataPropreP1$T22),table(dataPropreP2$T22))
+chi2<-chisq.test(table(dataPropreP1$T22),table(dataPropreP2$T22),correct = FALSE)
 chi2
 
 
@@ -76,32 +93,45 @@ chi2
 
 # T21
 mat<-cbind(matrix(table(dataPropreP1$T21)), matrix(table(dataPropreP2$T21)))
-prop.test(mat, correct = FALSE)
-
+prop.test(mat, alternative = "less", correct = FALSE)
+prop.test(mat, alternative = "greater", correct = FALSE)
+prop.test(mat, alternative = "two.sided", correct = FALSE)
 
 # T22
 mat<-cbind(matrix(table(dataPropreP1$T22)), matrix(table(dataPropreP2$T22)))
-prop.test(mat, correct = FALSE)
+prop.test(mat, alternative = "less", correct = FALSE)
+prop.test(mat, alternative = "greater", correct = FALSE) #0.07 p-value
+prop.test(mat, alternative = "two.sided", correct = FALSE)
 
 # T23
 mat<-cbind(matrix(table(dataPropreP1$T23)), matrix(table(dataPropreP2$T23)))
-prop.test(mat, correct = FALSE)
+prop.test(mat, alternative = "less", correct = FALSE)
+prop.test(mat, alternative = "greater", correct = FALSE)
+prop.test(mat, alternative = "two.sided", correct = FALSE)
 
 # T31
 mat<-cbind(matrix(table(dataPropreP1$T31)), matrix(table(dataPropreP2$T31)))
-prop.test(mat, correct = FALSE)
+prop.test(mat, alternative = "less", correct = FALSE)
+prop.test(mat, alternative = "greater", correct = FALSE)
+prop.test(mat, alternative = "two.sided", correct = FALSE)
 
 # T32
 mat<-cbind(matrix(table(dataPropreP1$T32)), matrix(table(dataPropreP2$T32)))
-prop.test(mat, correct = FALSE)
+prop.test(mat, alternative = "less", correct = FALSE)
+prop.test(mat, alternative = "greater", correct = FALSE)
+prop.test(mat, alternative = "two.sided", correct = FALSE)
 
 # T51
 mat<-cbind(matrix(table(dataPropreP1$T51)), matrix(table(dataPropreP2$T51)))
-prop.test(mat, correct = FALSE)
+prop.test(mat, alternative = "less", correct = FALSE)
+prop.test(mat, alternative = "greater", correct = FALSE)
+prop.test(mat, alternative = "two.sided", correct = FALSE)
 
 # T52
 mat<-cbind(matrix(table(dataPropreP1$T52)), matrix(table(dataPropreP2$T52)))
-prop.test(mat, correct = FALSE)
+prop.test(mat, alternative = "less", correct = FALSE)
+prop.test(mat, alternative = "greater", correct = FALSE)
+prop.test(mat, alternative = "two.sided", correct = FALSE)
 
 
 

8f7896a146ea9f8c0f9d4f93fb124423c7c626a7
Lucas91380 <lucas.chabeau@gmail.com>
2019-03-18 16:38:02 +0100

    HopHopHop ! Ca fonctionne (y'a que la cah pour l'instant)

--- app.R
+++ app.R
@@ -3,7 +3,7 @@ require(ggplot2)
 
 ############ Préparation des jeux de données dont nous aurons besoin ############
 
-#### Classification ascendante hiérarchique des variables ------------------------------
+#### Classification ascendante hiérarchique des variables ------------------------------#
 load("export/classif_questions.RData")
 
 
@@ -15,7 +15,7 @@ load("export/classif_questions.RData")
 ui <- fluidPage(
   
   #Titre de l'application
-  titlePanel("Données clim'Actions"),
+  titlePanel("Application Shiny projet Cogmont"),
   
   #Définition du layout de l'appli
   sidebarLayout(
@@ -25,21 +25,20 @@ ui <- fluidPage(
       
       #Menu déroulant pour choisir le jeu de données
       selectInput(
-        inputId="choix_ville",
-        label="Veuillez choisir une ville",
-        choices=c()
+        inputId = "choix_cah",
+        label = "Veuillez choisir une classification",
+        choices = ls()[which(substr(ls(),1,3)=="cah")]
       ),
       
-      #Menu déroulant pour choisir la variable
-      selectInput(
-        inputId="choix_variable",
-        label="Veuillez choisir une variable",
-        choices=names(donnees[,c(8,10,12,13)])
+      #Choix du nombre de classes
+      sliderInput(
+        inputId = "nb_class",
+        label = "Nombre de classes",
+        min = 2,
+        max = 20,
+        value = 7
       )
-      
     ),
-    
-    
     
     #Composants dans la fenêtre principale (au milieu)
     mainPanel(
@@ -60,38 +59,14 @@ server <- function(input, output) {
   #l'élément displot que nous avons créé dans ui
   output$distPlot <- renderPlot({
     
-    #On récupère la ville et la variable séléctionnée
-    ville<-input$choix_ville
-    variable<-input$choix_variable
-    
-    #date début, date fin
-    deb<-min(donnees[which(donnees$Ville==ville),4])
-    fin<-max(donnees[which(donnees$Ville==ville),4])
-    
-    #Transformation de notre variable en objet time series
-    variableTS<- ts(donnees[which(donnees$Ville==ville),which(names(donnees)==variable)], freq = 365, start = deb)
-    
-    #Aggrégation de notre jeu de données par année
-    variableTSBY<-aggregate(variableTS,nfrequency=1,FUN=mean)
-    
-    #Préparation jeu de donnees pour ggplot
-    dataplot<-as.data.frame(cbind(variableTSBY,c(deb:fin)))
-    names(dataplot)<-c("valeur","date")
-    
-    #Création du graphique
-    ggplot(dataplot,aes(date,valeur))+
-      geom_line(col="chartreuse4")+
-      geom_point(size=2,col="chartreuse3")+
-      geom_smooth(col="red")
+    #Affichage du dendogramme
+    plot(get(input$choix_cah))
+    rect.hclust(get(input$choix_cah),input$nb_class)
   })
 }
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------#
 
-
 # Run the application 
 shinyApp(ui = ui, server = server)
-
-#Supprssion des variables qui ne nous servent pas
-rm(list=setdiff(ls(), c("dataPropre", "dataSum", "dataVec", "don.groupe")))

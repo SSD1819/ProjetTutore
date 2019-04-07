e6c15692adddf96bb94a69f2c52e4076d65f8823
Etienne27 <etienne.hamard@outlook.fr>
2019-04-07 21:40:28 +0200

    ajout T1

--- Classif_Pedagogie.R
+++ Classif_Pedagogie.R
@@ -64,36 +64,36 @@ rect.hclust(cahSum.P2,6)
 #######################
 ########Sans T1
 #######################
-
-##Pour les données generales avec P1
-cahG.P1<-hclustvar(X.quali=dataQg.P1[,-c(1,2)])
-plot(cahG.P1, main="Dendogramme des donnees generales \n de la pedagogie P1")
-rect.hclust(cahG.P1,12)
-
-##Pour les données generales avec P2
-cahG.P2<-hclustvar(X.quali=dataQg.P2[,-c(1,2)])
-plot(cahG.P2, main="Dendogramme des donnees generales\n de la pedagogie P2")
-rect.hclust(cahG.P2,12)
-
-##Pour les données avec les vecteurs avec P1
-cahVec.P1<-hclustvar(X.quali=dataQVec.P1[,-c(1,2)])
-plot(cahVec.P1, main="Dendogramme des données avec les vecteurs\n de la pedagogie P1")
-rect.hclust(cahVec.P1,6)
-
-##Pour les données avec les vecteurs avec P2
-cahVec.P2<-hclustvar(X.quali=dataQVec.P2[,-c(1,2)])
-plot(cahVec.P2, main="Dendogramme des données avec les vecteurs\n de la pedagogie P2")
-rect.hclust(cahVec.P2,6)
-
-##Pour les données avec les sommes avec P1
-cahSum.P1<-hclustvar(X.quanti=dataQSum.P1[,-c(1,2,5,6)],X.quali=dataQSum.P1[,c(5,6)])
-plot(cahSum.P1, main="Dendogramme des données avec sommes \n de la pedagogie P1")
-rect.hclust(cahSum.P1,6)
-
-##Pour les données avec les sommes avec P2
-cahSum.P2<-hclustvar(X.quanti=dataQSum.P2[,-c(1,2,5,6)],X.quali=dataQSum.P2[,c(5,6)])
-plot(cahSum.P2, main="Dendogramme des données avec sommes\n de la pedagogie P2")
-rect.hclust(cahSum.P2,6)
+# 
+# ##Pour les données generales avec P1
+# cahG.P1<-hclustvar(X.quali=dataQg.P1[,-c(1,2)])
+# plot(cahG.P1, main="Dendogramme des donnees generales \n de la pedagogie P1")
+# rect.hclust(cahG.P1,12)
+# 
+# ##Pour les données generales avec P2
+# cahG.P2<-hclustvar(X.quali=dataQg.P2[,-c(1,2)])
+# plot(cahG.P2, main="Dendogramme des donnees generales\n de la pedagogie P2")
+# rect.hclust(cahG.P2,12)
+# 
+# ##Pour les données avec les vecteurs avec P1
+# cahVec.P1<-hclustvar(X.quali=dataQVec.P1[,-c(1,2)])
+# plot(cahVec.P1, main="Dendogramme des données avec les vecteurs\n de la pedagogie P1")
+# rect.hclust(cahVec.P1,6)
+# 
+# ##Pour les données avec les vecteurs avec P2
+# cahVec.P2<-hclustvar(X.quali=dataQVec.P2[,-c(1,2)])
+# plot(cahVec.P2, main="Dendogramme des données avec les vecteurs\n de la pedagogie P2")
+# rect.hclust(cahVec.P2,6)
+# 
+# ##Pour les données avec les sommes avec P1
+# cahSum.P1<-hclustvar(X.quanti=dataQSum.P1[,-c(1,2,5,6)],X.quali=dataQSum.P1[,c(5,6)])
+# plot(cahSum.P1, main="Dendogramme des données avec sommes \n de la pedagogie P1")
+# rect.hclust(cahSum.P1,6)
+# 
+# ##Pour les données avec les sommes avec P2
+# cahSum.P2<-hclustvar(X.quanti=dataQSum.P2[,-c(1,2,5,6)],X.quali=dataQSum.P2[,c(5,6)])
+# plot(cahSum.P2, main="Dendogramme des données avec sommes\n de la pedagogie P2")
+# rect.hclust(cahSum.P2,6)
 
 ####Exportation des data pour l'app Shiny####
 save(list=setdiff(ls(), c("dataPropre", "dataSum", "dataVec", "don.groupe", "dataSumOld", "dataVecOld")), file = "export/Classif_Pedagogie.RData")

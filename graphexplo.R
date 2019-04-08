f3beb8882bdb78446a76f69a9c8422587a2becee
MatFrancois <matthieu.francois@yahoo.fr>
2019-04-08 14:09:08 +0200

    push push push

--- graphexplo.R
+++ graphexplo.R
@@ -24,18 +24,33 @@ df<-data.frame(score=scoretotal,Pedagogie=dataPropre$Pedagogie,Annee=dataPropre$
 noms<-c("Pedagogie", "T1","T2","T3",
         "T5","T61","T62","T71","T72",
         "T8.123","T8.456789")
-score<-dataSum[noms]
+nom<-c("Pedagogie", "T1","T21","T22","T23","T31",
+       "T32","T41a","T41b","T41c","T41d","T42a",
+       "T42b","T42c","T42d","T51","T52","T61",
+       "T62","T71","T72","T81","T82","T83",
+       "T84","T85","T86","T87","T88","T89")
+dp<-dataPropre[,nom]
+score<-dataSum[,noms]
 boxplot(score[,-c(1,2)])
 
 ###changement de la df pour ggplot2
 # install.packages("reshape")
+require(ggplot2)
 library(reshape)
 md.df <- melt(score, id=(c("Pedagogie", "T1")))
-
+md.dfpropre <- melt(dp, id=(c("Pedagogie", "T1")))
+md.dfpropre$variable<-paste0(md.dfpropre$variable,"_",substr(md.dfpropre$Pedagogie,start = 1,stop = 1))
 ggplot(md.df,aes(variable,value)) + 
   geom_boxplot()
 
+ggplot(md.dfpropre, aes(x = variable, fill = value)) + 
+  geom_bar(position = "fill") +
+  # scale_color_manual(values = c("Conventionnelle" = "white"
+                                # , "Montessori" = "white")) + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+#soustraire le nombre de 1 pour chaque question entre montessori et conventionnelle
+  
 
 ####Exportation des data pour l'app Shiny####
 save(list=setdiff(ls(), c("dataPropre", "dataSum", "dataVec")), file = "export/graphexplo.RData")
